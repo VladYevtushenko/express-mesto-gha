65ebc4b4f7104cc9fe2864f43b760d55acfd3c66
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-07-01 21:12:33 +0300

    update routes with Joi

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,47 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { urlValidation } = require('../utils/urlValidation');
 
 const {
   getUsers,
   getUserId,
-  createUser,
   editUserInfo,
   editUserAvatar,
   getUser,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:userId', getUserId);
 router.get('/me', getUser);
-router.post('/', createUser);
-router.patch('/me', editUserInfo);
-router.patch('/me/avatar', editUserAvatar);
+
+router.get(
+  '/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().length(24).hex(),
+    }),
+  }),
+  getUserId,
+);
+
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  editUserInfo,
+);
+
+router.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().custom(urlValidation),
+    }),
+  }),
+  editUserAvatar,
+);
 
 module.exports = router;
