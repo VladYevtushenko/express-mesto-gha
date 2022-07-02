665be29e588f6d0446636c56401aade4258a96f1
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-07-03 00:02:24 +0300

    test attempt 5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,21 @@
 const jwt = require('jsonwebtoken');
+const UnauthorisedError = require('../errors/unauthorisedError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    throw new Error('');
+    // throw new Error('Ошибка авторизации');
+    next(new UnauthorisedError('Ошибка авторизации'));
+    return;
   }
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'secret-key');
   } catch (error) {
-    next(new Error('Необходима авторизация'));
+    next(new UnauthorisedError('Ошибка авторизации'));
+    return;
   }
   req.user = payload;
   next();
