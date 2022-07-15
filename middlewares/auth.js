a4c953ce2b67606966dd8a524086544ae01329e9
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-07-15 13:58:10 +0300

    temp res

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,26 @@
-const jwt = require('jsonwebtoken');
-const UnauthorisedError = require('../errors/unauthorisedError');
+// const jwt = require('jsonwebtoken');
+// const UnauthorisedError = require('../errors/unauthorisedError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt || res.token;
-  if (!token) {
-    next(new UnauthorisedError('Ошибка авторизации 321'));
-    return;
+  // const token = req.cookies.jwt || res.token;
+  res
+    .status(200)
+    .send({ req, res });
+  // if (!token) {
+  //   next(new UnauthorisedError('Ошибка авторизации 321'));
+  //   return;
+  // }
+  // let payload;
+  // try {
+  //   payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+  // } catch (error) {
+  //   next(new UnauthorisedError('Ошибка авторизации 123'));
+  //   return;
+  // }
+  // req.user = payload;
+  if (true === false) {
+    next();
   }
-  let payload;
-  try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
-  } catch (error) {
-    next(new UnauthorisedError('Ошибка авторизации 123'));
-    return;
-  }
-  req.user = payload;
-  next();
 };
