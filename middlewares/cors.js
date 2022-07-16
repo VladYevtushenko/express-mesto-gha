d94170e26ee3139f7b45ec86ce342ccc5366383c
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-07-16 14:19:26 +0300

    try to fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,9 +1,11 @@
 // const { allowedCors } = require('../utils/consts');
 const allowedCors = [
+  'mesto.by.vlad.nomorepartiesxyz.ru',
   'https://mesto.by.vlad.nomorepartiesxyz.ru',
   'http://mesto.by.vlad.nomorepartiesxyz.ru',
   'localhost:3000',
   'http://localhost:3000',
+  'https://localhost:3000',
 ];
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
